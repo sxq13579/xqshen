d863e0c9a69703e15a2b09b63c6b0dca992f30eb
shenxiaoqing <18137702933@163.com>
2018-01-22 10:05:14 +0800

    添加路由处理函数

--- js/server.js
+++ js/server.js
@@ -1,17 +1,19 @@
-var http = require('http');
-var url = require('url');
+var http = require("http");
+var url = require("url");
 
-function start() {
-	function onRequest(request, response) {
-		var pathname = url.parse(request.url).pathname;
-		console.log('Request for' + pathname + 'received');
-		response.writeHead(200, {'Content-Type': 'Text/plain'});
-		response.write('Hello World');
-		response.end();
-	}
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
 
-	http.createServer(onRequest).listen(8888);
-	console.log('Server has started');
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    var content = route(handle, pathname);
+    response.write(content);
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
 
-exports.start = start
+exports.start = start;
